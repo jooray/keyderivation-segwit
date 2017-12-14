c32a545a36fb40b07db149efb2348581e4e85a66
Juraj Bednar <juraj@bednar.io>
2017-12-14 11:53:25 +0100

    moved P2WPKH over P2SH to my fork of bip32utils

--- README.md
+++ README.md
@@ -5,13 +5,12 @@ over P2SH addresses from a master public key that is similar to
 [addrgen](https://github.com/prusnak/addrgen).
 
 This is my implementation in Python, it is not lightweight as it
-uses several other libraries, but it works. Pull requests to get
-rid of dependencies are welcome.
+uses [my fork of bip32utils](https://github.com/jooray/bip32utils),
+but it works. Pull requests to get rid of dependencies are welcome.
 
 # Dependencies
 
-If you want to be able to handle ypub keys, install my fork of
-bip32utils, otherwise you can just install the official version.
+Install my fork of bip32utils:
 
 ```bash
 pip3 install git+https://github.com/jooray/bip32utils --upgrade
@@ -19,8 +18,5 @@ pip3 install git+https://github.com/jooray/bip32utils --upgrade
 
 # Usage
 
-```python
-master_public_key='ypub6X3jfYGmBWr9oXXNfiJP13ydsSKjVWS1929vx6RAJdsFYMCZJeNrNf2tfEmNKkQ81xwD1E1xQZxrUWvgLousBuc3aec5YgDRrFfzBAP1VtN' # this can be xpub or ypub mpk
-address = derive_address(master_public_key, [0, 2]) # go for ./0/2 key derivation path
-print(address) # 38o7aArrsNZoZT4iXGrFrhxAKjfiRp8QX3
-```
+For usage example see addrgen.py
+
--- addrgen.py
+++ addrgen.py
@@ -1,27 +1,12 @@
-import hashlib
-from hashlib import sha256
 from bip32utils import Base58, BIP32Key
-
-def pk_to_p2wpkh_as_p2sh_addr(pk_bytes, testnet=False):
-    assert len(pk_bytes) == 33 and (pk_bytes.startswith(b"\x02") or pk_bytes.startswith(b"\x03")), \
-        "Only compressed public keys are compatible with p2sh-p2wpkh addresses. " \
-        "See https://github.com/bitcoin/bips/blob/master/bip-0049.mediawiki."
-    pk_hash = hash160_bytes(pk_bytes)
-    push_20 = bytes.fromhex("0014")
-    script_sig = push_20 + pk_hash
-    address_bytes = hash160_bytes(script_sig)
-    prefix = b"\xc4" if testnet else b"\x05"
-    address = Base58.check_encode(prefix + address_bytes)
-    return address
-
-
-def hash160_bytes(byte_input):
-    return hashlib.new('ripemd160', sha256(byte_input).digest()).digest()
 
 def derive_address(mpk, path = []):
     pub = BIP32Key.fromExtendedKey(mpk)
     for child_index in path:
         pub = pub.ChildKey(child_index)
-    return pk_to_p2wpkh_as_p2sh_addr(pub.PublicKey())
+    return pub.P2WPKHoP2SHAddress()
 
+master_public_key='ypub6X3jfYGmBWr9oXXNfiJP13ydsSKjVWS1929vx6RAJdsFYMCZJeNrNf2tfEmNKkQ81xwD1E1xQZxrUWvgLousBuc3aec5YgDRrFfzBAP1VtN' # this can be xpub or ypub mpk
+address = derive_address(master_public_key, [0, 2]) # go for ./0/2 key derivation path
+print(address) # 38o7aArrsNZoZT4iXGrFrhxAKjfiRp8QX3
 
